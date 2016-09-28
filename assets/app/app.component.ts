c34958757b0e864c8a555f8788aaa71798b0a903
Vlad <vieira83@gmail.com>
2016-09-28 12:17:00 -0400

    Added component message list

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,34 +1,23 @@
 import {Component} from 'angular2/core';
-import {MessageComponent} from './messages/message.component';
-import {Message} from "./messages/message"
+import {MessageListComponent} from './messages/message-list.component';
 
 @Component({
     selector: 'my-app',
     template: `
     <div class="row">
-    	<section class="col-md-8 col-md-offset-2">
-    		<!-- ngModel  - keyword or directive that allows data binding , it allows two types of binding: property binding and even binding, uses both to listen 
-    		<input type="text" [(ngModel)]="message.content"/> -->
-    		
-    	</section>
+    	<!--<section class="col-md-8 col-md-offset-2">
+    		 ngModel  - keyword or directive that allows data binding , it allows two types of binding: property binding and even binding, uses both to listen 
+    		<input type="text" [(ngModel)]="message.content"/>
+    	</section>-->
     </div>
     <div class="row">
-	    <section  class="col-md-8 col-md-offset-2">
-	    	<my-message *ngFor="#message of messages" [message]="message" (editClickd)="message.content=$event"></my-message>  
-	    	<!-- tag that the message component will use 
-	    	#message- creates a local variable storing the data, you can access it all over the template, message no longer refers to aproperty but now a local variable
-	    	-->
-	    </section> 
+	   <my-message-list></my-message-list>
     </div>	
     `,
-    directives: [MessageComponent]
+    directives: [MessageListComponent]
 })
 // out component has a property called message
 // The message property is echoed in the template
 export class AppComponent {
-	message: Message [] = [
-		new Message("A new Message", null, 'Max'),
-		new Message('Second Message', null, 'Vlad')
-
-	]
+	
 }
